4d5fb2e30972c0aff4817f90424e5d653864787d
Lameche06 <mapourecedric96@gmail.com>
2024-08-05 00:41:32 +0200

    add component on system

--- src/views/admin/system/index.js
+++ src/views/admin/system/index.js
@@ -1,9 +1,27 @@
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import Card from 'components/card/Card'
 import React from 'react'
+import Entitynew from './Component/Entitynew'
 
 const System = () => {
   return (
     <div>
-      
+      <Card mt="100px">
+        <Tabs variant='soft-rounded' colorScheme='green'>
+          <TabList>
+            <Tab>ENTITY</Tab>
+            <Tab>PROFIL</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Entitynew />
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Card>
     </div>
   )
 }
